ea0781367b224b0310349b7bef7b549b75a1d4e6
muskanmansoori <muskanmansoori258@gmail.com>
2026-08-23 19:34:10 +0530

    add

--- app/admin/(admin)/page.jsx
+++ app/admin/(admin)/page.jsx
@@ -1,4 +1,42 @@
-const admin = ()=>{
+"use client";
+
+import { useAuth } from "@/store/auth";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+const Admin = () => {
+  const { user, isLoading } = useAuth();
+  const router = useRouter();
+
+  console.log("ADMIN USER =", user);
+  console.log("IS ADMIN =", user?.isAdmin);
+
+  useEffect(() => {
+    if (isLoading) return;
+
+    if (!user) {
+      router.replace("/");
+      return;
+    }
+
+    if (user.user.isAdmin !== true) {
+      router.replace("/");
+    }
+  }, [user, isLoading, router]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!user || user.user.isAdmin !== true) {
     return null;
-}
-export default admin;
+  }
+
+  return (
+    <div>
+      Admin Dashboard
+    </div>
+  );
+};
+
+export default Admin;
